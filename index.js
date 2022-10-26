6e378e32f4cd11bfb0b179b919b4edae44881400
BayezidMostafa <hiperx1@gmail.com>
2022-10-26 19:15:52 +0600

    Find out single data by id using find method

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 const course_data = require('./data/course_data.json')
 app.use(cors());
-app.get('/courses', (req, res) => {
-    res.send(course_data);
-})
 app.get('/', (req, res) => {
     res.send('My Server is running')
 })
+
+app.get('/courses', (req, res) => {
+    res.send(course_data);
+})
+
+app.get('/courses/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = course_data.find(cd => cd.id === id)
+    res.send(selectedCourse);
+})
+
 app.listen(port, () => {
     console.log(`My Learning Pool server is running on Port ${port}`);
 })
