10363cde4c4f40f16ad28a3baa324524afaaa129
BayezidMostafa <hiperx1@gmail.com>
2022-10-26 14:29:36 +0600

    Courses Data API Created

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-
-
+const course_data = require('./data/course_data.json')
 app.use(cors());
-
+app.get('/courses', (req, res) => {
+    res.send(course_data);
+})
 app.get('/', (req, res) => {
     res.send('My Server is running')
 })
-
-
 app.listen(port, () => {
     console.log(`My Learning Pool server is running on Port ${port}`);
 })
